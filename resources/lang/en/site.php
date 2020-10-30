04e2898b55a507732a2f88df00d9262eba50067e
Mohamed Saad <eng.mohameds2016@gmail.com>
2020-10-30 19:45:09 +0200

    auth with facebook and send email

--- resources/lang/en/site.php
+++ resources/lang/en/site.php
@@ -1,12 +1,33 @@
 <?php
 
 return[
-    'username' => 'Username',
-    'email' => 'Email',
-    'password' => 'Password',
-    'remember-me' => 'Remember-me',
-    'signin' => 'Sign up',
-    'img' => 'Image',
+    
+    'username_required'     => 'must enter username',
+    'username_unique'       => 'sorry, this username already exists ',
+    'email_required'        => 'must enter email ',
+    'email_unique'          => 'sorry, this email already exists ',
+    'email_email'           => 'must enter correct email',
+    'password_required'     => 'must enter password',
+    'password_min'          => 'sorry , must password is strong and less than 8 chars',
+    'img_required'          => 'must enter image ',
+    'img_image'             => 'must enter correct image ',
+    'img_mimes'             => 'must enter image  [jpg , jpeg , png , svg]',
+
+    'username'              => 'Username',
+    'email'                 => 'Email',
+    'password'              => 'Password',
+    'remember-me'           => 'Remember-me',
+    'signin'                => 'Sign up',
+    'img'                   => 'Image',
+
+    'successadduser'        => 'Success register',
+    'login'                 => 'Login',
+    'email_not_exists'      => 'This email not exist',
+    'loginfacebook'         => ' login using facebook ',
+    'confirmRegister'       => ' confirm register ',
+    'confirm'               => ' confirm ',
+    'donesendemail'         => ' done send confirmation msg in your email ',
+    'errorcode'             => ' this code not correct  ',
 ];
 
 
